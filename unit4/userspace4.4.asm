cc4dbb2368a99303aed45700bc8ee284fd446f71
Sen Lin <lin0385@flinders.edu.au>
2020-09-15 22:25:03 +0930

    testing

--- unit4/userspace4.4.asm
+++ unit4/userspace4.4.asm
@@ -7,27 +7,53 @@ main: {
     message: .text "printed via print string api"
     .byte 0
 }
+// print_string(byte* zeropage(2) message)
 print_string: {
-    //*(unsigned char *)$0300 = *message;
-    //*(unsigned char *)$0301 = 't';
-    //*(unsigned char *)$0302 = 'e';
-    //*(unsigned char *)$0303 = 's';
-    //*(unsigned char *)$0304 = 't';
-    //*(unsigned char *)$0305 = message[2];
-    //*(unsigned char *)$0306 = message[255];
-    .label location = $300
-    ldx #0
-  //while (*message){
-  //	*(location + i) = *message++;
-  //	i++;
-  //}
-  //while (message){
-  //	*(location+i) = message[i];
-  //	i++;
-  //}
+    .label location = 4
+    .label message = 2
+    lda #<$300
+    sta.z location
+    lda #>$300
+    sta.z location+1
+    lda #<main.message
+    sta.z message
+    lda #>main.message
+    sta.z message+1
+  __b1:
+    ldy #0
+    lda (message),y
+    cmp #0
+    bne __b2
+    tya
+    tay
+    sta (location),y
+    jsr call_syscall02
+    rts
   __b2:
-    lda main.message,x
-    sta location,x
-    inx
-    jmp __b2
+    ldy #0
+    lda (message),y
+    sta (location),y
+    inc.z location
+    bne !+
+    inc.z location+1
+  !:
+    inc.z message
+    bne !+
+    inc.z message+1
+  !:
+    jmp __b1
+}
+call_syscall02: {
+    jsr enable_syscalls
+    lda #1
+    sta $d642
+    nop
+    rts
+}
+enable_syscalls: {
+    lda #$47
+    sta $d02f
+    lda #$53
+    sta $d02f
+    rts
 }
--- unit4/userspace4.4.vs
+++ unit4/userspace4.4.vs
@@ -1,6 +1,10 @@
+al C:86d .enable_syscalls
+al C:83e .__b1
+al C:863 .call_syscall02
 al C:82e .print_string
 al C:80b .upstartEnd
 al C:80d .main
-al C:300 .location
-al C:811 .message
-al C:830 .__b2
+al C:4 .location
+al C:811 .message__0
+al C:2 .message__1
+al C:84e .__b2
